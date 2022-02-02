8f2d5632e3aeac83f18d021ac3908ecef9bdc2d7
FlutLab.io <>
2022-02-02 14:54:37 +0000

    Autosave

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -6,6 +6,20 @@ class NewTransaction extends StatelessWidget {
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
+  void submitData(String val) {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      titleController.text,
+      double.parse(amountController.text),
+    );
+  }
+
   NewTransaction(this.addTx);
   @override
   Widget build(BuildContext context) {
@@ -19,6 +33,7 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
+              onSubmitted: (_) => submitData,
               // onChanged: (val) {
               //   titleInput = val;
               // },
@@ -28,16 +43,12 @@ class NewTransaction extends StatelessWidget {
               // onChanged: (val) => amountInput = val,
               controller: amountController,
               keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData('Dummy'),
             ),
             FlatButton(
               child: Text('Add Transaction'),
               textColor: Colors.purple,
-              onPressed: () {
-                addTx(
-                  titleController.text,
-                  double.parse(amountController.text),
-                );
-              },
+              onPressed: () => submitData,
             ),
           ],
         ),
