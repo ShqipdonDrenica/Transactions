bf2b58708052ee705992a59028763f1bbd09742b
FlutLab.io <>
2022-02-03 13:56:19 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
       title: 'Personal Expenses',
       theme: ThemeData(
         primarySwatch: Colors.purple,
-        accentColor: Colors.purple,
+        accentColor: Colors.yellow,
       ),
       home: MyHomePage(),
     );
