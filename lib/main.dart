dc27315dfbe7947f408cc43a38ad0484d26420be
FlutLab.io <>
2022-02-03 10:34:58 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -89,7 +89,6 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             TransactionList(_userTransactions),
-            UserTransaction(),
           ],
         ),
       ),
