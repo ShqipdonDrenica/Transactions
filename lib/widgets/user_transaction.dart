91ed24789a82d976cb8fcbd61e6d4af87d6f0468
FlutLab.io <>
2022-02-03 10:29:36 +0000

    Autosave

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import './transaction_list.dart';
 import '../models/transaction.dart';
 import 'new_transaction.dart';
+import '../main.dart';
 
 class UserTransaction extends StatefulWidget {
   @override
@@ -9,39 +10,11 @@ class UserTransaction extends StatefulWidget {
 }
 
 class _UserTransactionState extends State<UserTransaction> {
-  final List<Transaction> _userTransactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
-  void _addNewTransaction(String txTitle, double txAmount) {
-    final newTx = Transaction(
-      title: txTitle,
-      amount: txAmount,
-      date: DateTime.now(),
-      id: DateTime.now().toString(),
-    );
-
-    setState(() {
-      _userTransactions.add(newTx);
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         NewTransaction(_addNewTransaction),
-        TransactionList(_userTransactions),
       ],
     );
   }
