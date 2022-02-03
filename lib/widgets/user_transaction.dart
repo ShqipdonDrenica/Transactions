8343751d2e173a2e80cfb785872a1be6cea0aef4
FlutLab.io <>
2022-02-03 10:31:53 +0000

    Autosave

--- lib/widgets/user_transaction.dart
+++ lib/widgets/user_transaction.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
-import './transaction_list.dart';
-import '../models/transaction.dart';
-import 'new_transaction.dart';
-import '../main.dart';
+// import 'package:flutter/material.dart';
+// import './transaction_list.dart';
+// import '../models/transaction.dart';
+// import 'new_transaction.dart';
+// import '../main.dart';
 
-class UserTransaction extends StatefulWidget {
-  @override
-  _UserTransactionState createState() => _UserTransactionState();
-}
+// class UserTransaction extends StatefulWidget {
+//   @override
+//   _UserTransactionState createState() => _UserTransactionState();
+// }
 
-class _UserTransactionState extends State<UserTransaction> {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        NewTransaction(_addNewTransaction),
-      ],
-    );
-  }
-}
+// class _UserTransactionState extends State<UserTransaction> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       children: [
+//         NewTransaction(_addNewTransaction),
+//       ],
+//     );
+//   }
+// }
